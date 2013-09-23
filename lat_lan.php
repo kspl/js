cf95add84baf36d714674d3be793159861c0ca20
kspl <email.ahire@gmail.com>
2013-09-23 17:11:46 +0530

    5
    hfhfhfh

--- lat_lan.php
+++ lat_lan.php
@@ -1,6 +1,6 @@
 <?php   
 
-/* 111111111111111111112222
+/* 99966
 $zip = '1323 rg';
 $site = file_get_contents('http://geocoder.ca/?postal='.$zip, false, NULL, 1000, 1000);
 $goods = strstr($site, 'GPoint('); // cut off the first part up until 
